6c0dbb2f2e4d4b8eb59a613473402ad1044c10c5
Rocky Green <rockjock51@gmail.com>
2019-05-27 22:42:47 -0500

    Day 57: Switched Back to Highcharts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Visuals from './components/Visuals';
+import Visuals from "./components/Visuals";
+
 import "./App.css";
 
 function App() {
--- src/components/Visuals.js
+++ src/components/Visuals.js
@@ -1,506 +1,169 @@
 import React from "react";
 
-import { Container, Paper, Typography, Grid } from "@material-ui/core";
-import { ResponsiveLine } from "@nivo/line";
+import { Container, Paper, Grid } from "@material-ui/core";
 
-const data = [
-  {
-    id: "japan",
-    color: "hsl(259, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 295
-      },
-      {
-        x: "helicopter",
-        y: 19
-      },
-      {
-        x: "boat",
-        y: 111
-      },
-      {
-        x: "train",
-        y: 190
-      },
-      {
-        x: "subway",
-        y: 82
-      },
-      {
-        x: "bus",
-        y: 229
-      },
-      {
-        x: "car",
-        y: 42
-      },
-      {
-        x: "moto",
-        y: 191
-      },
-      {
-        x: "bicycle",
-        y: 163
-      },
-      {
-        x: "horse",
-        y: 214
-      },
-      {
-        x: "skateboard",
-        y: 12
-      },
-      {
-        x: "others",
-        y: 253
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+import "../css/Visuals.css";
+
+class Visuals extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          zoomType: "x"
+        },
+        title: {
+          text: "Watts"
+        },
+        tooltip: {
+          formatter: function() {
+            return (
+              "Date: " +
+              Highcharts.dateFormat("%b %e %Y %H:%M:%S ", new Date(this.x)) +
+              "<br />Volts: " +
+              this.point.Volts +
+              "<br />Amps: " +
+              this.point.Amps
+            );
+          }
+        },
+        subtitle: {
+          text:
+            document.ontouchstart === undefined
+              ? "Click and drag in the plot area to zoom in"
+              : "Pinch the chart to zoom in"
+        },
+        xAxis: {
+          type: "datetime"
+        },
+        yAxis: {
+          title: {
+            text: "Watts"
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          area: {
+            fillColor: {
+              linearGradient: {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 1
+              },
+              stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [
+                  1,
+                  Highcharts.Color(Highcharts.getOptions().colors[0])
+                    .setOpacity(0)
+                    .get("rgba")
+                ]
+              ]
+            },
+            marker: {
+              radius: 2
+            },
+            lineWidth: 1,
+            states: {
+              hover: {
+                lineWidth: 1
+              }
+            },
+            threshold: 0,
+            negativeColor: "#800000",
+            negativeFillColor: {
+              linearGradient: {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 1
+              },
+              stops: [[0, "#FFFFFF"], [1, "#F08080"]]
+            }
+          }
+        },
+        series: [
+          {
+            connectNulls: true,
+            turboThreshold: 0,
+            parseDate: function(val) {
+              console.log(val);
+              console.log("Date", Date.parse(val));
+              return Date.parse(val);
+            },
+            type: "area",
+            name: "Watts",
+            data: null
+          }
+        ]
       }
-    ]
-  },
-  {
-    id: "france",
-    color: "hsl(263, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 182
-      },
-      {
-        x: "helicopter",
-        y: 80
-      },
-      {
-        x: "boat",
-        y: 173
-      },
-      {
-        x: "train",
-        y: 88
-      },
-      {
-        x: "subway",
-        y: 208
-      },
-      {
-        x: "bus",
-        y: 296
-      },
-      {
-        x: "car",
-        y: 25
-      },
-      {
-        x: "moto",
-        y: 14
-      },
-      {
-        x: "bicycle",
-        y: 9
-      },
-      {
-        x: "horse",
-        y: 223
-      },
-      {
-        x: "skateboard",
-        y: 296
-      },
-      {
-        x: "others",
-        y: 163
-      }
-    ]
-  },
-  {
-    id: "us",
-    color: "hsl(323, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 93
-      },
-      {
-        x: "helicopter",
-        y: 272
-      },
-      {
-        x: "boat",
-        y: 227
-      },
-      {
-        x: "train",
-        y: 133
-      },
-      {
-        x: "subway",
-        y: 60
-      },
-      {
-        x: "bus",
-        y: 40
-      },
-      {
-        x: "car",
-        y: 243
-      },
-      {
-        x: "moto",
-        y: 148
-      },
-      {
-        x: "bicycle",
-        y: 16
-      },
-      {
-        x: "horse",
-        y: 137
-      },
-      {
-        x: "skateboard",
-        y: 182
-      },
-      {
-        x: "others",
-        y: 115
-      }
-    ]
-  },
-  {
-    id: "germany",
-    color: "hsl(76, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 274
-      },
-      {
-        x: "helicopter",
-        y: 55
-      },
-      {
-        x: "boat",
-        y: 17
-      },
-      {
-        x: "train",
-        y: 228
-      },
-      {
-        x: "subway",
-        y: 20
-      },
-      {
-        x: "bus",
-        y: 268
-      },
-      {
-        x: "car",
-        y: 291
-      },
-      {
-        x: "moto",
-        y: 213
-      },
-      {
-        x: "bicycle",
-        y: 214
-      },
-      {
-        x: "horse",
-        y: 262
-      },
-      {
-        x: "skateboard",
-        y: 88
-      },
-      {
-        x: "others",
-        y: 117
-      }
-    ]
-  },
-  {
-    id: "norway",
-    color: "hsl(73, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 226
-      },
-      {
-        x: "helicopter",
-        y: 220
-      },
-      {
-        x: "boat",
-        y: 115
-      },
-      {
-        x: "train",
-        y: 11
-      },
-      {
-        x: "subway",
-        y: 51
-      },
-      {
-        x: "bus",
-        y: 67
-      },
-      {
-        x: "car",
-        y: 258
-      },
-      {
-        x: "moto",
-        y: 294
-      },
-      {
-        x: "bicycle",
-        y: 98
-      },
-      {
-        x: "horse",
-        y: 192
-      },
-      {
-        x: "skateboard",
-        y: 266
-      },
-      {
-        x: "others",
-        y: 250
-      }
-    ]
+    };
   }
-];
 
-const Visuals = () => {
-  return (
-    <div>
-      <Container maxWidth="false" height={300}>
-        <Grid container spacing={3}>
-          <Grid item lg={4}>
-            <Paper style={{ height: "30em" }}>
-              <Typography variant="h6" color="inherit">
-                Chart 1!
-              </Typography>
-              <ResponsiveLine
-                data={data}
-                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                xScale={{ type: "point" }}
-                yScale={{
-                  type: "linear",
-                  stacked: true,
-                  min: "auto",
-                  max: "auto"
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                  orient: "bottom",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "transportation",
-                  legendOffset: 36,
-                  legendPosition: "middle"
-                }}
-                axisLeft={{
-                  orient: "left",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "count",
-                  legendOffset: -40,
-                  legendPosition: "middle"
-                }}
-                colors={{ scheme: "nivo" }}
-                pointSize={10}
-                pointColor={{ theme: "background" }}
-                pointBorderWidth={2}
-                pointBorderColor={{ from: "serieColor" }}
-                pointLabel="y"
-                pointLabelYOffset={-12}
-                useMesh={true}
-                legends={[
-                  {
-                    anchor: "bottom-right",
-                    direction: "column",
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: "left-to-right",
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: "circle",
-                    symbolBorderColor: "rgba(0, 0, 0, .5)",
-                    effects: [
-                      {
-                        on: "hover",
-                        style: {
-                          itemBackground: "rgba(0, 0, 0, .03)",
-                          itemOpacity: 1
-                        }
-                      }
-                    ]
-                  }
-                ]}
-              />
-            </Paper>
+  handleDataRefresh() {
+    fetch("http://solar-cert:3050/api/stats/watts")
+      .then(response => response.json())
+      .then(newData => {
+        this.setState(prevState => ({
+          options: {
+            ...prevState.options,
+            series: {
+              ...prevState.series,
+              data: newData
+            }
+          }
+        }));
+      });
+  }
+
+  componentDidMount() {
+    this.handleDataRefresh();
+    setInterval(() => {
+      this.handleDataRefresh();
+    }, 30000);
+  }
+
+  render() {
+    return (
+      <div>
+        <Container maxwidth="false" height={300} className={"grid"}>
+          <Grid container spacing={1}>
+            <Grid item lg={4}>
+              <Paper style={{ height: "30em" }}>
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={this.state.options}
+                />
+              </Paper>
+            </Grid>
+            <Grid item lg={4}>
+              <Paper style={{ height: "30em" }}>
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={this.state.options}
+                />
+              </Paper>
+            </Grid>
+            <Grid item lg={4}>
+              <Paper style={{ height: "30em" }}>
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={this.state.options}
+                />
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item lg={4}>
-            <Paper maxWidth="s" style={{ height: "30em" }}>
-              <Typography variant="h6" color="inherit">
-                Chart 1!
-              </Typography>
-              <ResponsiveLine
-                data={data}
-                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                xScale={{ type: "point" }}
-                yScale={{
-                  type: "linear",
-                  stacked: true,
-                  min: "auto",
-                  max: "auto"
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                  orient: "bottom",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "transportation",
-                  legendOffset: 36,
-                  legendPosition: "middle"
-                }}
-                axisLeft={{
-                  orient: "left",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "count",
-                  legendOffset: -40,
-                  legendPosition: "middle"
-                }}
-                colors={{ scheme: "nivo" }}
-                pointSize={10}
-                pointColor={{ theme: "background" }}
-                pointBorderWidth={2}
-                pointBorderColor={{ from: "serieColor" }}
-                pointLabel="y"
-                pointLabelYOffset={-12}
-                useMesh={true}
-                legends={[
-                  {
-                    anchor: "bottom-right",
-                    direction: "column",
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: "left-to-right",
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: "circle",
-                    symbolBorderColor: "rgba(0, 0, 0, .5)",
-                    effects: [
-                      {
-                        on: "hover",
-                        style: {
-                          itemBackground: "rgba(0, 0, 0, .03)",
-                          itemOpacity: 1
-                        }
-                      }
-                    ]
-                  }
-                ]}
-              />
-            </Paper>
-          </Grid>
-          <Grid item lg={4}>
-            <Paper maxWidth="s" style={{ height: "30em" }}>
-              <Typography variant="h6" color="inherit">
-                Chart 1!
-              </Typography>
-              <ResponsiveLine
-                data={data}
-                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                xScale={{ type: "point" }}
-                yScale={{
-                  type: "linear",
-                  stacked: true,
-                  min: "auto",
-                  max: "auto"
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                  orient: "bottom",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "transportation",
-                  legendOffset: 36,
-                  legendPosition: "middle"
-                }}
-                axisLeft={{
-                  orient: "left",
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "count",
-                  legendOffset: -40,
-                  legendPosition: "middle"
-                }}
-                colors={{ scheme: "nivo" }}
-                pointSize={10}
-                pointColor={{ theme: "background" }}
-                pointBorderWidth={2}
-                pointBorderColor={{ from: "serieColor" }}
-                pointLabel="y"
-                pointLabelYOffset={-12}
-                useMesh={true}
-                legends={[
-                  {
-                    anchor: "bottom-right",
-                    direction: "column",
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: "left-to-right",
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: "circle",
-                    symbolBorderColor: "rgba(0, 0, 0, .5)",
-                    effects: [
-                      {
-                        on: "hover",
-                        style: {
-                          itemBackground: "rgba(0, 0, 0, .03)",
-                          itemOpacity: 1
-                        }
-                      }
-                    ]
-                  }
-                ]}
-              />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
-};
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Visuals;
